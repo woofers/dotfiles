253e99a43726b8dfa769863d8e879c76464feb17
Jaxson Van Doorn <woofers1tt@gmail.com>
2017-10-03 20:19:15 -0700

    Added ZSH as default shell

--- wsltty/local/wsl~.bat
+++ wsltty/local/wsl~.bat
@@ -14,6 +14,6 @@ rem To enable invocation of this script from WIN+R or from cmd.exe,
 rem you may want to copy this script into "%SYSTEMROOT%\System32",
 rem or include the installation folder "%LOCALAPPDATA%\wsltty" in your PATH
 
-"%installdir%\bin\mintty.exe" --wsl -h err "--configdir=%configdir%" -o Locale=C -o Charset=UTF-8 -i "%LOCALAPPDATA%\wsltty\wsl.ico" /bin/wslbridge -C/mnt/d/Documents/Development/src -t /bin/bash
+"%installdir%\bin\mintty.exe" --wsl -h err "--configdir=%configdir%" -o Locale=C -o Charset=UTF-8 -i "%LOCALAPPDATA%\wsltty\wsl.ico" /bin/wslbridge -C/mnt/d/Documents/Development/src -t /bin/zsh
 
 
