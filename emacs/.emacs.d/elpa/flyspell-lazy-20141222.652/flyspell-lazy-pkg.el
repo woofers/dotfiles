364e7bc26e2ffd5b74443acd0e21573de34aa626
Jaxson Van Doorn <woofers1tt@gmail.com>
2017-10-24 00:01:28 -0700

    Flyspell

---
+++ emacs/.emacs.d/elpa/flyspell-lazy-20141222.652/flyspell-lazy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flyspell-lazy" "20141222.652" "Improve flyspell responsiveness using idle timers" 'nil :commit "31786fe04a4732d2f845e1c7e96fcb030182ef10" :url "http://github.com/rolandwalker/flyspell-lazy" :keywords '("spelling"))
---
+++ emacs/.emacs.d/elpa/flyspell-popup-20170529.115/flyspell-popup-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flyspell-popup" "20170529.115" "Correcting words with Flyspell in popup menus" '((popup "0.5.0")) :commit "29311849bfd253b9b689bf331860b4c4d3bd4dde" :url "https://github.com/xuchunyang/flyspell-popup" :keywords '("convenience"))
