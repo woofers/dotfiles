1922d1eac19d6ca85ef3dc4ca11df27090889f68
Jaxson Van Doorn <woofers1tt@gmail.com>
2017-11-14 19:47:18 -0800

    Org Mode Config

--- emacs/.emacs.d/lisp/init-org.el
+++ emacs/.emacs.d/lisp/init-org.el
@@ -1,10 +1,4 @@
 
-;;
-;; init-org.el
-;; Config File for Org Mode
-;;
-
-;; Install Org Related Plug-Ins
 (use-package org
     :ensure t)
 
@@ -23,55 +17,38 @@
 (use-package ox-beamer
     :ensure t)
 
-;; Agenda
-(setq org-agenda-files '("D:/Documents/JVD Docs/Documents/School/UVIC/2017"))
 (setq org-agenda-window-setup (quote current-window))
 
 (setq org-blank-before-new-entry (quote ((heading) (plain-list-item))))
 
-;; Only Allow Item to Marked a Done
-;; When all Children are Done
 (setq org-enforce-todo-dependencies t)
+
+(setq org-agenda-files '("D:/Documents/JVD Docs/Documents/School/UVIC/2017"))
 
 ;; (setq org-ellipsis " ⤵")
 (setq org-ellipsis ".")
-(setq org-src-fontify-natively t)
-(setq org-src-tab-acts-natively t)
-(setq org-hide-leading-stars t)
-(setq org-src-window-setup 'current-window)
+
+(setq org-todo-keywords
+    '((sequence "STUDY" "TODO" "IN-PROGRESS" "NEAR-COMPLETION"
+                "WAITING" "REVIEW" "|" "DONE" "CANCELED")))
 
 (setq org-log-done (quote time))
 (setq org-log-redeadline (quote time))
 (setq org-log-reschedule (quote time))
 
-;; Org Keys Words
-(setq org-todo-keywords
-      '((sequence "STUDY" "TODO" "IN-PROGRESS" "NEAR-COMPLETION"
-                  "WAITING" "REVIEW" "|" "DONE" "CANCELED")))
+(setq org-src-fontify-natively t)
+(setq org-src-tab-acts-natively t)
+(setq org-hide-leading-stars t)
+(setq org-src-window-setup 'current-window)
 
 (defun org-mark-done-and-archive ()
-  "Mark the state of an org-mode item as DONE and archive it."
-  (interactive)
-  (org-todo 'done)
-  (org-archive-subtree))
+    "Mark the state of an org-mode item as DONE and archive it."
+    (interactive)
+    (org-todo 'done)
+    (org-archive-subtree))
 
-(org-add-link-type
- "image-url"
- (lambda (path)
-   (let ((img (expand-file-name
-           (concat (md5 path) "." (file-name-extension path))
-           temporary-file-directory)))
-     (if (file-exists-p img)
-     (find-file img)
-       (url-copy-file path img)
-       (find-file img)))))
-
-
-;; Keymaps for Agenda and Links
 (define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
 
-;; Enable Bullets
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(define-key global-map "\C-ca" 'org-agenda)
 
 (provide 'init-org)
