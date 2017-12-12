fed65937d55a85a4b25152d809a6e118accb94ed
Jaxson Van Doorn <woofers1tt@gmail.com>
2017-12-11 23:36:48 -0800

    Added Better Eshell Prompt

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -14,12 +14,14 @@
     ("D:/Documents/JVD Docs/Documents/School/UVIC/2017/To Do.org")))
  '(package-selected-packages
    (quote
-    (esup haxe-imports haxe-mode helm-projectile projectile ox-beamer ox-md w3m pdf-tools fireplace imenu-list highlight-escape-sequences markdown-mode git-gutter-fringe org-bullets org-plus-contrib evil-jumper which-key use-package telephone-line smart-tabs-mode powerline-evil neotree magit load-dir highlight-symbol helm evil-tabs evil-surround evil-leader evil-indent-textobject))))
+    (eshell-git-prompt multi-term esup haxe-imports haxe-mode helm-projectile projectile ox-beamer ox-md w3m pdf-tools fireplace imenu-list highlight-escape-sequences markdown-mode git-gutter-fringe org-bullets org-plus-contrib evil-jumper which-key use-package telephone-line smart-tabs-mode powerline-evil neotree magit load-dir highlight-symbol helm evil-tabs evil-surround evil-leader evil-indent-textobject))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(eshell-git-prompt-powerline-dir-face ((t (:background "#FF5555"))))
+ '(eshell-git-prompt-powerline-not-clean-face ((t (:background "#ffaf00"))))
  '(fringe ((t (:inherit default :foreground "#1b182c"))))
  '(helm-header ((t (:inherit header-line))))
  '(helm-source-header ((t (:foreground "#cbe3e7" :slant oblique :weight ultra-bold :height 1.75 :width extra-expanded :family "Arial"))))
@@ -32,21 +34,14 @@
  '(powerline-evil-operator-face ((t (:inherit powerline-evil-base-face :background "#6f60ff"))))
  '(powerline-evil-replace-face ((t (:inherit powerline-evil-base-face :background "#414048"))))
  '(powerline-evil-visual-face ((t (:inherit powerline-evil-base-face :background "#f39918" :foreground "#100e23"))))
- '(speck-mouse ((t (:inherit highlight)))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ '(speck-mouse ((t (:inherit highlight))))
  '(term-color-black ((t (:foreground "#282A36"))))
- '(term-color-red ((t (:foreground "#FF5555"))))
- '(term-color-green ((t (:foreground "#50FA7B"))))
- '(term-color-yellow ((t (:foreground "#F1FA8C"))))
  '(term-color-blue ((t (:foreground "#CAA9FA"))))
  '(term-color-cyan ((t (:foreground "#FF79C6"))))
+ '(term-color-green ((t (:foreground "#50FA7B"))))
  '(term-color-magenta ((t (:foreground "#8BE9FD"))))
+ '(term-color-red ((t (:foreground "#FF5555"))))
  '(term-color-white ((t (:foreground "#E6E6E6"))))
- '(term-default-fg-color ((t (:inherit term-color-white))))
+ '(term-color-yellow ((t (:foreground "#F1FA8C"))))
  '(term-default-bg-color ((t (:inherit term-color-black))))
- )
+ '(term-default-fg-color ((t (:inherit term-color-white)))))
