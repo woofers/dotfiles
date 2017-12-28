e20ab74b158472f13fbcc3459bacebddc15a8567
Jaxson Van Doorn <woofers1tt@gmail.com>
2017-12-27 23:30:55 -0800

    Install GnuTLS (Resolve #2)

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -14,15 +14,15 @@
     ("D:/Documents/JVD Docs/Documents/School/UVIC/2017/To Do.org")))
  '(package-selected-packages
    (quote
-    (speck flyspell-lazy wttrin gradle-mode pcomplete-extension highlight-indent-guides highlight nlinum-relative auto-complete ranger diminish powerline ox-twbs evil-magit evil eshell-git-prompt multi-term esup haxe-imports haxe-mode helm-projectile projectile ox-beamer ox-md w3m pdf-tools fireplace imenu-list highlight-escape-sequences markdown-mode git-gutter-fringe org-bullets org-plus-contrib evil-jumper which-key use-package telephone-line smart-tabs-mode powerline-evil neotree magit load-dir highlight-symbol helm evil-tabs evil-surround evil-leader evil-indent-textobject))))
+    (evil-mu4e speck flyspell-lazy wttrin gradle-mode pcomplete-extension highlight-indent-guides highlight nlinum-relative auto-complete ranger diminish powerline ox-twbs evil-magit evil eshell-git-prompt multi-term esup haxe-imports haxe-mode helm-projectile projectile ox-beamer ox-md w3m pdf-tools fireplace imenu-list highlight-escape-sequences markdown-mode git-gutter-fringe org-bullets org-plus-contrib evil-jumper which-key use-package telephone-line smart-tabs-mode powerline-evil neotree magit load-dir highlight-symbol helm evil-tabs evil-surround evil-leader evil-indent-textobject))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(eshell-git-prompt-powerline-clean-face ((t (:foreground "#1b182c" :background "#50fa7b"))))
  '(eshell-git-prompt-powerline-dir-face ((t (:foreground "#1b182c" :background "#FF5555"))))
  '(eshell-git-prompt-powerline-not-clean-face ((t (:foreground "#1b182c" :background "#ffaf00"))))
- '(eshell-git-prompt-powerline-clean-face ((t (:foreground "#1b182c" :background "#50fa7b"))))
  '(fringe ((t (:inherit default :foreground "#1b182c"))))
  '(helm-header ((t (:inherit header-line))))
  '(helm-source-header ((t (:foreground "#cbe3e7" :slant oblique :weight ultra-bold :height 1.75 :width extra-expanded :family "Arial"))))
