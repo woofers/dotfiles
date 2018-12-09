4dafa65c05e32af2042b6681e79ca86e0ed27f51
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-12-08 22:36:28 -0800

    Install LaTeX science package

--- install.sh
+++ install.sh
@@ -141,6 +141,7 @@ sudo apt-get install texlive-base
 sudo apt-get install texlive-latex-base
 sudo apt-get install texlive-latex-extra
 sudo apt-get install texlive-fonts-recommended
+sudo apt-get install texlive-science
 sudo apt-get install texlive-bibtex-extra texlive-binaries texlive-font-utils
 
 # Install mbsync
