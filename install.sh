3a5466052175083a6be80bf4e6a0b549068b5859
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-04-04 16:30:24 -0700

    Fix install deps typo

--- install.sh
+++ install.sh
@@ -249,7 +249,7 @@ sudo apt-get install libnotify-bin
 sudo apt-get install scrot
 
 # Picom
-sudo apt-get isntall ibxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev  libpcre2-dev  libevdev-dev uthash-dev libev-dev libx11-xcb-dev libev-libevent-dev libev4
+sudo apt-get install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev  libpcre2-dev  libevdev-dev uthash-dev libev-dev libx11-xcb-dev libev-libevent-dev libev4
 cd ~/Downloads
 git clone https://github.com/yshui/picom
 cd picom
